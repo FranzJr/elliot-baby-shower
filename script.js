8e74bfbd8c8da4264f108f49dccbd001788f66ae
Franz Joseph <frogelez@zenware.com.co>
2023-04-14 08:48:38 -0500

    Versión Inicial 0.0.0

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    const regalos = [
+        "Cuna",
+        "Carrito",
+        // Agrega aquí las 18 opciones adicionales de regalos
+    ];
+
+    $("#ruleta").click(function () {
+        const regaloSeleccionado = regalos[Math.floor(Math.random() * regalos.length)];
+        $("#regaloElegido").text(`Regalo elegido: ${regaloSeleccionado}`);
+        $("#regalo").val(regaloSeleccionado);
+    });
+
+    $("#invitacion").submit(function (event) {
+        event.preventDefault();
+
+        const nombre = $("#nombre").val();
+        const acompanantes = $("#acompanantes").val();
+        const regalo = $("#regalo").val();
+
+        // Asegúrate de tener configurado el acceso a la API de Google Sheets en tu proyecto
+        const url = "https://sheets.googleapis.com/v4/spreadsheets/[ID_DE_HOJA_DE_CÁLCULO]/values/A1:append?valueInputOption=USER_ENTERED&access_token=[TOKEN_DE";
+        // Reemplaza [ID_DE_HOJA_DE_CÁLCULO] y [TOKEN_DE_ACCESO] con los valores correspondientes de tu proyecto
+
+        const datos = {
+            "range": "A1",
+            "majorDimension": "ROWS",
+            "values": [
+                [nombre, acompanantes, regalo]
+            ]
+        };
+
+        $.ajax({
+            url: url,
+            method: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(datos),
+            success: function (response) {
+                alert("Asistencia confirmada y regalo seleccionado guardados");
+                $("#invitacion").trigger("reset");
+                $("#regaloElegido").text("");
+            },
+            error: function (error) {
+                alert("Hubo un error al guardar la información. Por favor, inténtalo de nuevo.");
+            }
+        });
+
+    });
+});
